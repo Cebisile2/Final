a0f762ab5941f6b18d92404d30b4e7410e47a35f
Constantine <Constantine>
2025-10-07 11:10:19 +0200

    replace firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPrcYnBDz3CK8a1QByXIGGCxh59fEaKoM",
-  authDomain: "myrealworld-1.firebaseapp.com",
-  projectId: "myrealworld-1",
-  storageBucket: "myrealworld-1.appspot.com",
-  messagingSenderId: "991368400501",
-  appId: "1:991368400501:web:4fa635a6329432fd38f23c",
-  measurementId: "G-F77198LPT4",
+  apiKey: "AIzaSyCe0bcXJ2mGT_CCnLVe7NyQuAdlyIO_8Tk",
+  authDomain: "academiatrack-7f5af.firebaseapp.com",
+  projectId: "academiatrack-7f5af",
+  storageBucket: "academiatrack-7f5af.appspot.com",
+  messagingSenderId: "771911203416",
+  appId: "1:771911203416:web:f60b946280a136aaa2e82d",
+  measurementId: "G-D8FRB7RY2Q"
 };
 
 // Initialize Firebase
